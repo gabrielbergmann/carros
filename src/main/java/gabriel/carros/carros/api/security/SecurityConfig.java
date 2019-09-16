c290b32bc8082a347dafdf4d44cb418907ad32b6
Gabriel Bergmann <gabriel.bergmann@senior.com.br>
2019-09-16 11:59:44 -0300

    Autentication

--- src/main/java/gabriel/carros/carros/api/security/SecurityConfig.java
+++ src/main/java/gabriel/carros/carros/api/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package gabriel.carros.carros.api.security;
+package com.carros.api.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,21 +15,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().csrf().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         auth
-                .inMemoryAuthentication()
+                .inMemoryAuthentication().passwordEncoder(encoder)
                 .withUser("user").password(encoder.encode("user")).roles("USER")
                 .and()
                 .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
