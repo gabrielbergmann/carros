f6f927f513982de9679e4896208dd4c51c084553
Gabriel Bergmann <gabriel.bergmann@senior.com.br>
2019-09-19 11:34:39 -0300

    Config para deploy

--- src/main/java/gabriel/carros/carros/domain/Carro.java
+++ src/main/java/gabriel/carros/carros/domain/Carro.java
@@ -13,9 +13,9 @@ public class Carro {
     private Long id;
     private String nome;
     private String tipo;
-//    private String descricao;
-//    private String urlFoto;
-//    private String urlVideo;
-//    private String latitude;
-//    private String longitude;
+    private String descricao;
+    private String urlFoto;
+    private String urlVideo;
+    private String latitude;
+    private String longitude;
 }
